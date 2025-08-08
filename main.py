0d528a9e5abcaab581c4e3103097603fab54aa2e
levgirgin <lev.girgin@gmail.com>
2025-08-08 12:02:46 +0200

    Example2

---
+++ main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+
+def load_data(file_path):
+    """
+    Load data from a CSV file into a pandas DataFrame.
+    """
+    try:
+        data = pd.read_csv(file_path)
+        return data
+    except Exception as e:
+        print(f"Error loading data: {e}")
+        return None
+    
+    def division(a, b):
+        return a/b
